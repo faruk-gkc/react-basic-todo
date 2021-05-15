b89a0c7d07eebdd8dc8a4ac9bb7cb0a0ad51d7f8
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-05-15 15:41:06 +0300

    Header semantic-ui'a göre ayarlandı.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+
 const Header = () => {
   return (
-    <Navbar fixed="top" expand="lg" variant="light" bg="light" className="mb-4">
-      <Container>
-            <h2>Contact Manager</h2>
-      </Container>
-    </Navbar>
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
   );
 };
 
