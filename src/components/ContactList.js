0112280e83f08e497d7db5fc2f003ccf38339f5f
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-05-15 15:34:33 +0300

    contactList added.

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import {ListGroup} from 'react-bootstrap'
+import React from "react";
+import ContactCard from "./ContactCard";
 
-const ContactList = () => {
+const ContactList = (props) => {
+  console.log(props);
+
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
     return (
-        <ListGroup>
-            Contact List
-        </ListGroup>
-    )
-}
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
 
-export default ContactList
+export default ContactList;
