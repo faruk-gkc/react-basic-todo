b4dbfac5bc17228ae9397b8c756991184fa17db0
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-05-15 14:19:25 +0300

    component'ler , components klasörü içerisine aktarıldı.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
+import 'semantic-ui-css/semantic.min.css'
 
 
 function App() {
